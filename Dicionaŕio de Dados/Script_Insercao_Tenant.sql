253ab3db40fb90fa5805e6d6f31184cc3764dda0
Gustavo Trausula <gustavo.trausula@itbxtech.com.br>
2026-04-15 10:14:35 -0300

    Script de inserção de tenants
    Criação do script de inserção de dados sintéticos de contabilidades.

---
+++ Dicionaŕio de Dados/Script_Insercao_Tenant.sql
@@ -0,0 +1,6 @@
+-- Inserção dos dados da tabela Tenant
+
+INSERT INTO tenant (tenantdesc,tenantcnpj) VALUES
+("Contabilidade 1","57.700.455/0001-37"),
+("Contabilidade 2","66.727.098/0001-91"),
+("Contabilidade 3","95.615.510/0001-80");
